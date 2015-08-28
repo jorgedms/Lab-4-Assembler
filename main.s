dc1729d1b24305e43a710e069fd6b475c536d73b
aju13171 <c.valentinaju@gmail.com>
2015-08-28 07:58:09 -0600

    macro Write, macro encendido1 y macro encendido2
    los tres macros a usar

--- main.s
+++ main.s
@@ -33,6 +33,44 @@ b main
 	mov r1, #1
 	bl SetGpioFunction
 .endm
+
+@------------------------------------------------------------------
+@Macro para encendido de 2 leds
+@Recibe:
+@		puerto1 = puerto a encender
+@		puerto2 = puerto a encender
+@Funcion:
+@		Enciende los pines recibidos y crea un delay de 1 segundo
+@------------------------------------------------------------------
+.macro Encendido2 puerto1, puerto2
+	/*encendido de pines*/
+	mov r0, \puerto1	@puerto a encender
+	mov r1, #1			@codigo de encendido
+	bl SetGpio
+	
+	mov r0, \puerto2	@puerto a encender
+	mov r1, #1			@codigo de encendido
+	bl SetGpio
+	
+	/*delay*/
+	ldr r0, =1000000
+	bl Wait
+.endm
+
+@------------------------------------------------------------------
+@Macro para encendido de 1 led
+@Recibe:
+@		puerto1 = puerto a encender
+@Funcion:
+@		Enciende el pin que recibe
+@------------------------------------------------------------------
+.macro Encendido1 puerto1
+	/*encender pin*/
+	mov r0, \puerto1	@puerto a encender
+	mov r1, #1			@codigo de encendido
+	bl SetGpio
+.endm
+
 @---------------------------------------------------------------
 @					fin de seccion de Macros
 @---------------------------------------------------------------
