dc419360a9db42e70876a3c5f8f243e425465dbd
aju13171 <c.valentinaju@gmail.com>
2015-08-28 07:36:20 -0600

    main.s
    archivo main

---
+++ main.s
@@ -0,0 +1,38 @@
+@--------------------------------------------------------
+@Universidad del Valle de Guatemala
+@Prelaboratorio 4 Assembler
+@Autor: Christopher Aju, 13171
+@
+@Archivo que realiza una secuencia de encendido de leds
+@primera secuencia
+@		verde:encendido
+@		amarillo:apagado
+@		rojo:apagado
+@
+@segunda secuencia
+@		verde:apagado
+@		amarillo:encendido
+@		rojo:apagado
+@
+@tercera secuencia
+@		verde:apagado
+@		amarillo:apagado
+@		rojo:encendido
+@--------------------------------------------------------
+
+.section .init
+.globl _start
+_start:
+
+b main
+@---------------------------------------------------------------
+@Macro para el write
+@---------------------------------------------------------------
+
+@---------------------------------------------------------------
+@					fin de seccion de Macros
+@---------------------------------------------------------------
+.section .text
+
+main:
+	mov sp, #0x8000
